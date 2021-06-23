41158189ecd6d1930c238c8e230c2106f6216d68
Vishnu Anand <vishnuanand2000@gmail.com>
2021-06-23 19:27:11 +0530

    Initialized UI elements

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //XML resource declarations
+    TextView BMI_Display;
+    EditText UserWeight, UserFoot, UserInches;
+    Button CalculateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //XML resource initializations
+        BMI_Display = findViewById(R.id.BMI_Output);
+        UserWeight = findViewById(R.id.UserInput_Weight);
+        UserFoot = findViewById(R.id.UserInput_Foot);
+        UserInches = findViewById(R.id.UserInput_Inches);
+        CalculateButton = findViewById(R.id.CalculateButton);
+
+
     }
 }
