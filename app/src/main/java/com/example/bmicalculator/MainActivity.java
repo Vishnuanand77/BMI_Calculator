90bed3019bc35a02a70c3f577228efa8d6323fb9
Vishnu Anand <vishnuanand2000@gmail.com>
2021-06-23 21:09:45 +0530

    Added Weight Slider

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.bmicalculator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -12,12 +13,20 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.google.android.material.slider.Slider;
+
+import org.jetbrains.annotations.NotNull;
+
 public class MainActivity extends AppCompatActivity {
 
     //XML resource declarations
-    TextView BMI_Display;
-    EditText UserWeight, UserFoot, UserInches;
+    TextView BMI_Display, WeightDisplay;
+    EditText UserInches;
     Button CalculateButton;
+
+    //Slider
+    Slider WeightSlider;
+    double UserWeight = 0;
 
 
     @Override
@@ -27,32 +36,45 @@ public class MainActivity extends AppCompatActivity {
 
         //XML resource initializations
         BMI_Display = findViewById(R.id.BMI_Output);
-        UserWeight = findViewById(R.id.UserInput_Weight);
-//        UserFoot = findViewById(R.id.UserInput_Foot);
+        WeightDisplay = findViewById(R.id.WeightDisplay);
         UserInches = findViewById(R.id.UserInput_Inches);
         CalculateButton = findViewById(R.id.CalculateButton);
 
-        //Array adapter for the spinner
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.HeightInFoot, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Slider
+        WeightSlider = findViewById(R.id.WeightSlider);
 
+        WeightSlider.addOnSliderTouchListener(new Slider.OnSliderTouchListener() {
 
-
-        CalculateButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                //Converting User Input into doubles
-                double Weight = Float.parseFloat(UserWeight.getText().toString());
-                double HeightInFeet = Float.parseFloat(UserFoot.getText().toString()) * 0.3048; //Converting Foot to meters
-                double HeightInInch = Float.parseFloat(UserInches.getText().toString()) * 0.0254; //Converting Inches to meters
+            public void onStartTrackingTouch(@NonNull @NotNull Slider slider) {
 
-                Log.d("BMI Calculator :", String.valueOf(HeightInFeet));
-                Log.d("BMI Calculator :", String.valueOf(HeightInInch));
-
-                //Calculating BMI
-                calculateBMI(Weight, HeightInFeet, HeightInInch);
             }
+
+            @Override
+            public void onStopTrackingTouch(@NonNull @NotNull Slider slider) {
+                UserWeight = Float.parseFloat(String.valueOf(slider.getValue()));
+                WeightDisplay.setText(String.valueOf(UserWeight));
+                Log.d("BMI Calculator : Slider Value", String.valueOf(UserWeight));
+            }
+
         });
+
+
+//        CalculateButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //Converting User Input into doubles
+//                double Weight = Float.parseFloat(UserWeight.getText().toString());
+//                double HeightInFeet = Float.parseFloat(UserFoot.getText().toString()) * 0.3048; //Converting Foot to meters
+//                double HeightInInch = Float.parseFloat(UserInches.getText().toString()) * 0.0254; //Converting Inches to meters
+//
+//                Log.d("BMI Calculator :", String.valueOf(HeightInFeet));
+//                Log.d("BMI Calculator :", String.valueOf(HeightInInch));
+//
+//                //Calculating BMI
+//                calculateBMI(Weight, HeightInFeet, HeightInInch);
+//            }
+//        });
     }
 
     private void calculateBMI(double Weight, double HeightInFeet, double HeightInInch) {
