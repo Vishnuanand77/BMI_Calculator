eaf3ab6680b22aa3a8395a0e6063131cc40d693e
Vishnu Anand <vishnuanand2000@gmail.com>
2021-06-23 19:44:54 +0530

    Created the math logic to calculate BMI

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,6 +28,28 @@ public class MainActivity extends AppCompatActivity {
         UserInches = findViewById(R.id.UserInput_Inches);
         CalculateButton = findViewById(R.id.CalculateButton);
 
+        CalculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Converting User Input into doubles
+                double Weight = Float.parseFloat(UserWeight.getText().toString());
+                double HeightInFeet = Float.parseFloat(UserFoot.getText().toString()) * 0.3048; //Converting Foot to meters
+                double HeightInInch = Float.parseFloat(UserInches.getText().toString()) * 0.0254; //Converting Inches to meters
 
+                Log.d("BMI Calculator :", String.valueOf(HeightInFeet));
+                Log.d("BMI Calculator :", String.valueOf(HeightInInch));
+
+                double totalHeight = (HeightInFeet + HeightInInch);
+
+                double squaredHeight = Math.pow(totalHeight, 2); //Squaring the height to get meter squared
+                Log.d("BMI Calculator :", String.valueOf(squaredHeight));
+
+                double BMI = Weight/squaredHeight;
+                Log.d("BMI Calculator :", String.valueOf(BMI));
+
+                //Assigning BMI value to TextView
+                BMI_Display.setText(String.valueOf(BMI));
+            }
+        });
     }
 }
